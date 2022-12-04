b3074f1ee091197f4ce926e17a27962ffc06789c
Ben Groehbiel <benjamin.groehbiel@gmail.com>
2022-12-04 14:28:15 +0000

    Downgrade to elixir 1.13
    Signed-off-by: Jacek Rzeniewicz <jacek@rzrz.pl>

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PairDance.MixProject do
     [
       app: :pair_dance,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
