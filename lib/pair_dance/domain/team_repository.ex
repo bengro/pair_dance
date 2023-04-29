2051bbdafe7401bfaa2e89002d6a19de7be03ac4
Jacek Rzeniewicz <jacek@rzrz.pl>
2023-04-29 13:09:25 +0100

    Fix type annotations

--- lib/pair_dance/domain/team_repository.ex
+++ lib/pair_dance/domain/team_repository.ex
@@ -3,7 +3,8 @@ defmodule PairDance.Domain.TeamRepository do
   alias PairDance.Domain.Team
 
 
-  @callback create(String.t()) :: Team.t()
+  @callback create(String.t()) :: {:ok, Team.t()}
 
-  @callback find(integer()) :: Team.t()
+  @callback find(integer()) :: Team.t() | nil
+
 end
