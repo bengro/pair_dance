b4bad36b37ca1e4159c1782684d28cb84f2efc70
Ben Groehbiel <benjamin.groehbiel@gmail.com>
2023-07-08 07:39:57 +0100

    Tidy up service account configuration

--- infrastructure/container_registry.tf
+++ infrastructure/container_registry.tf
@@ -2,26 +2,3 @@ resource "google_container_registry" "pair_dance_registry" {
   project  = var.gcp_project_id
   location = "EU"
 }
-
-resource "google_service_account" "container_registry_user" {
-  project      = var.gcp_project_id
-  account_id   = "container-registry-user"
-  display_name = "Container registry read/write"
-}
-
-resource "google_service_account_key" "container_registry_key" {
-  service_account_id = google_service_account.container_registry_user.name
-}
-
-output "registry_service_account_private_key" {
-  sensitive = true
-  value     = google_service_account_key.container_registry_key.private_key
-}
-
-output "registry_service_account_public_key" {
-  value = google_service_account_key.container_registry_key.public_key
-}
-
-output "registry_service_account_id" {
-  value = google_service_account_key.container_registry_key.id
-}
--- infrastructure/service_accounts.tf
+++ infrastructure/service_accounts.tf
@@ -1,3 +1,9 @@
+resource "google_service_account" "ci" {
+  project      = var.gcp_project_id
+  account_id   = "ci-service-account"
+  display_name = "Service account with permissions to push images and deploy to cloud run"
+}
+
 resource "google_project_iam_binding" "service-account" {
   project = var.gcp_project_id
 
@@ -9,6 +15,23 @@ resource "google_project_iam_binding" "service-account" {
   role = each.key
 
   members = [
-    "serviceAccount:${google_service_account.container_registry_user.email}"
+    "serviceAccount:${google_service_account.ci.email}"
   ]
+}
+
+resource "google_service_account_key" "service_account_key" {
+  service_account_id = google_service_account.ci.name
+}
+
+output "ci_service_account_private_key" {
+  sensitive = true
+  value     = google_service_account_key.service_account_key.private_key
+}
+
+output "ci_service_account_public_key" {
+  value = google_service_account_key.service_account_key.public_key
+}
+
+output "ci_service_account_id" {
+  value = google_service_account_key.service_account_key.id
 }
