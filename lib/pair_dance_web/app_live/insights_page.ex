ab1be85fc1b40fc67258c37d13e59c747631db4a
Ben Groehbiel <benjamin.groehbiel@gmail.com>
2023-12-03 21:10:37 +0000

    Improve insights

--- lib/pair_dance_web/app_live/insights_page.ex
+++ lib/pair_dance_web/app_live/insights_page.ex
@@ -1,8 +1,9 @@
 defmodule PairDanceWeb.AppLive.InsightsPage do
   use PairDanceWeb, :live_view
 
+  alias PairDance.Domain.Insights.Report
   alias PairDance.Infrastructure.Team.EctoRepository, as: TeamRepository
-  alias PairDance.Infrastructure.Insights.Repository, as: WorkLogService
+  alias PairDance.Infrastructure.Insights.Repository, as: InsightRepository
   alias PairDance.Domain.Insights.Calendar
 
   import PairDanceWeb.AppLive.InsightsPage.Components
@@ -12,11 +13,11 @@ defmodule PairDanceWeb.AppLive.InsightsPage do
     user = session["current_user"]
     team = TeamRepository.find_by_slug?(slug)
 
-    {:ok, tasks_member_was_involved} = WorkLogService.get_assigned_tasks_by_user(user, team)
+    {:ok, tasks_member_was_involved} = InsightRepository.get_assigned_tasks_by_user(user, team)
     calendar = Calendar.build(tasks_member_was_involved)
 
-    {:ok, all_assignments} = WorkLogService.get_assignments_by_team(team)
-    report = PairDance.Domain.Insights.Report.generate_report(all_assignments)
+    {:ok, all_assignments} = InsightRepository.get_assignments_by_team(team)
+    report = Report.generate_report(all_assignments)
 
     assigns =
       socket
